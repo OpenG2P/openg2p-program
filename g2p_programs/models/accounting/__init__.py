5eca89864588abcee1c2e942ff899556412c4aab
Jeremi Joslin <jeremi@newlogic.com>
2022-08-23 05:43:56 +0200

    add g2p_programs

---
+++ g2p_programs/models/accounting/__init__.py
@@ -0,0 +1,5 @@
+# Part of OpenG2P. See LICENSE file for full copyright and licensing details.
+
+from . import account_journal
+from . import fund_management
+from . import fund_report
---
+++ g2p_programs/tests/__init__.py
@@ -0,0 +1,5 @@
+# Part of OpenG2P. See LICENSE file for full copyright and licensing details.
+
+from . import test_programs_id_eligibility_dedup
+from . import test_programs_phone_eligibility_deduplicate
+from . import test_programs
