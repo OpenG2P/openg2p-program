f1f8f3ccc161a36a7ed3beebd3033db03070c820
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2023-01-04 16:46:31 +0700

    [ADD] new feature to view queue jobs from program & cycle

---
+++ g2p_programs/models/job_relate_mixin.py
@@ -0,0 +1,28 @@
+from odoo import models
+
+
+class JobRelateMixin(models.AbstractModel):
+    _name = "job.relate.mixin"
+    _description = "Job Relate Mixin"
+
+    def action_view_related_queue_jobs(self):
+        """Create an action for opening a related queue jobs in form view
+        Usage:
+            - Create/ Inherit a model which can be delay when a function is
+            called.
+
+            - Set inherit to this model
+            ```python
+            _inherit = ["job.relate.mixin", ...]
+            ```
+            - Create a button to call this method
+
+        Returns:
+            _type_: dict
+        """
+        self.ensure_one()
+        jobs = self.env["queue.job"].sudo().search([("model_name", "=", self._name)])
+        related_jobs = jobs.filtered(lambda qj: self in qj.records)
+        action = self.env.ref("queue_job.action_queue_job").read()[0]
+        action["domain"] = [("id", "in", related_jobs.ids)]
+        return action
