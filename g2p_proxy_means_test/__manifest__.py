e408123d596200a9bc7fb5bf8331ae1975972ef4
mkumar-02 <mkumar6@ch.iitr.ac.in>
2023-03-31 12:22:47 +0530

    initial commit

---
+++ g2p_proxy_means_test/__manifest__.py
@@ -0,0 +1,25 @@
+# Part of OpenG2P Registry. See LICENSE file for full copyright and licensing details.
+{
+    "name": "G2P: Proxy Means Test",
+    "category": "G2P",
+    "version": "15.0.1.1.0",
+    "sequence": 1,
+    "author": "OpenG2P",
+    "website": "https://github.com/openg2p/openg2p-program",
+    "license": "Other OSI approved licence",
+    "development_status": "Alpha",
+    "maintainers": ["jeremi", "gonzalesedwin1123", "emjay0921"],
+    "depends": ["g2p_programs"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/program_pmt_config.xml",
+        "views/proxy_means_test_configuration.xml",
+        "views/program_registrant_info.xml",
+    ],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
