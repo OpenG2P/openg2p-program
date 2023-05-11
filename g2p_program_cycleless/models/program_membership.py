8a214bcf15e7d755788982dc3a2d823f8dbddd21
manoj-02 <mkumar6@ch.iitr.ac.in>
2023-05-11 16:40:13 +0530

    Modified active cycle logic for entitlement

--- g2p_program_cycleless/models/program_membership.py
+++ g2p_program_cycleless/models/program_membership.py
@@ -4,6 +4,17 @@ from odoo import models
 class G2PProgramMembership(models.Model):
     _inherit = "g2p.program_membership"
 
+    def active_cycle(self):
+        if not self.program_id.default_active_cycle.id:
+            self.program_id.create_new_cycle()
+
+            cycle_id = self.env["g2p.cycle"].search(
+                [("program_id", "=", self.program_id.id)]
+            )
+            return cycle_id.id
+
+        return self.program_id.default_active_cycle.id
+
     def open_entitlement_form_wizard(self):
 
         return {
@@ -19,7 +30,6 @@ class G2PProgramMembership(models.Model):
                 "default_partner_id": self.partner_id.id,
                 "default_program_id": self.program_id.id,
                 "default_currency_id": self.program_id.journal_id.currency_id.id,
-                "default_cycle_id": self.program_id.default_active_cycle.id,
-                "default_is_cash_entitlement": True,
+                "default_cycle_id": self.active_cycle(),
             },
         }
