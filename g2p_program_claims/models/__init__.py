6f649b9b948ad4488af270f680b6047edb34c472
Lalith Kota <kotalalith@gmail.com>
2023-05-19 13:36:00 +0530

    Removed claims programs in favor of Reimbursement programs

--- g2p_program_claims/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
-from . import programs
-from . import entitlement
-from . import cycle
-from . import entitlement_manager
