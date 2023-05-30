78d7cd02aa918d5a458dc8d5fb80e384068aeb24
Lalith Kota <kotalalith@gmail.com>
2023-05-30 11:11:20 +0530

    Removed empty model files

--- g2p_program_approval/models/__init__.py
+++ g2p_program_approval/models/__init__.py
@@ -1,5 +1,3 @@
 from . import approval_mapping
-from . import constants
 from . import manager
-from . import program
 from . import entitlement
--- g2p_program_assessment/models/__init__.py
+++ g2p_program_assessment/models/__init__.py
@@ -1,4 +1,2 @@
-from . import constants
 from . import assessment
-from . import program
 from . import program_membership
