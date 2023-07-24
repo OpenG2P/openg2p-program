738ff0e136c2d27e0469e815f99e936884faec8d
Dibik <dibik@openg2p.org>
2023-07-24 20:08:19 +0530

    G2P-216: Enhancement: Edit button should be removed in Cycle detailed view page once the Cycle is approved.

--- g2p_program_reimbursement/models/cycle.py
+++ g2p_program_reimbursement/models/cycle.py
@@ -8,6 +8,11 @@ class G2PCycle(models.Model):
         related="program_id.is_reimbursement_program"
     )
 
+    edit_css = fields.Html(
+        sanitize=False,
+        compute="_compute_css",
+    )
+
     def open_cycle_form(self):
         res = super(G2PCycle, self).open_cycle_form()
         if self.is_reimbursement_program:
@@ -15,3 +20,13 @@ class G2PCycle(models.Model):
                 "g2p_program_reimbursement.view_cycle_reimbursement_form"
             ).id
         return res
+
+    def _compute_css(self):
+        for rec in self:
+            # To Remove Edit Option
+            if rec.state not in "draft":
+                rec.edit_css = (
+                    "<style>.o_form_button_edit {display: none !important;}</style>"
+                )
+            else:
+                rec.edit_css = False
