3dad2f0465ef2658e4ad10a61b9fdb2f55fbe660
Lalith Kota <kotalalith@gmail.com>
2023-07-28 22:51:50 +0530

    Fixed Dev status and regenated Readmes

--- g2p_proxy_means_test/__manifest__.py
+++ g2p_proxy_means_test/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "OpenG2P",
     "website": "https://openg2p.org",
     "license": "Other OSI approved licence",
-    "development_status": "Alpha",
+    "development_status": "Production/Stable",
     "depends": ["g2p_programs", "g2p_program_registrant_info"],
     "data": [
         "security/ir.model.access.csv",
