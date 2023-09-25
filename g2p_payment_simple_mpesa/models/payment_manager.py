ad48d26a3bbedc3cba727b1b84663810d387ca5f
Abhishek Wagh <abhishek.created20@gmail.com>
2023-09-25 19:22:18 +0530

    Added endpoint for payment

--- g2p_payment_simple_mpesa/models/payment_manager.py
+++ g2p_payment_simple_mpesa/models/payment_manager.py
@@ -1,5 +1,6 @@
 # Part of OpenG2P. See LICENSE file for full copyright and licensing details.
 import logging
+import requests
 
 from odoo import api, fields, models
 
@@ -23,7 +24,9 @@ class PaymentManager(models.Model):
 
 class G2PPaymentManagerSimpleMpesa(models.Model):
     _name = "g2p.program.payment.manager.simple.mpesa"
-    _inherit = "g2p.program.payment.manager.default"
+    _inherit = ["g2p.program.payment.manager.default",
+                "mail.thread",
+                "mail.activity.mixin"]
     _description = "Simple MPesa Payment Manager"
 
     batch_tag_ids = fields.Many2many(
@@ -34,6 +37,21 @@ class G2PPaymentManagerSimpleMpesa(models.Model):
     )
 
     create_batch = fields.Boolean("Automatically Create Batch", default=True)
+    payee_id_type = fields.Selection(
+            [
+                ("bank_acc_no", "Bank Account Number"),
+                ("bank_acc_iban", "IBAN"),
+                ("phone", "Phone"),
+                ("email", "Email"),
+                ("reg_id", "Registrant ID"),
+                ],
+                "Payee ID Field",
+                required=True,
+    )
+    reg_id_type_for_payee_id = fields.Many2one(
+        "g2p.id.type", "Payee DFSP ID Type", required=False
+    )
+    customer_type = fields.Char(default = "subscriber")
 
     auth_endpoint_url = fields.Char("Authentication Endpoint URL", required=True)
     payment_endpoint_url = fields.Char("Payment Endpoint URL", required=True)
@@ -51,5 +69,70 @@ class G2PPaymentManagerSimpleMpesa(models.Model):
                 batch.batch_has_started = True
 
             # TODO: Implement Logic
+            # Perform Auth Here
+            try:
+                data = {
+                    "email": self.username,
+                    "password": self.password
+                }
+                headers = {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+                
+                response = requests.post(self.auth_endpoint_url, data=data, headers=headers)
+                response.raise_for_status()
+                response_data = response.json()
+                auth_token = response_data.get("token")
+
+                for payment in batch.payment_ids:
+                    payee_id_value = self._get_dfsp_id_and_type(payment)
+                    amount = payment.amount_issued
+                    auth_header = "Bearer " + auth_token
+                    # Define the headers
+                    headers = {
+                        "Authorization": auth_header,
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    }
+                    data = {
+                        "amount": amount,
+                        "accountNo": payee_id_value,
+                        "customerType": self.customer_type
+                    }
+                    try:
+                        response = requests.post(self.payment_endpoint_url, headers=headers, data=data)            
+                        _logger.info("MPesa Payment Transfer response", response.content)
+                        response.raise_for_status()
+                    except Exception as e:
+                        _logger.error("Mpesa Payment Failed with unknown reason",e)
+                        error_msg = "Mpesa Payment Failed during transfer with unknown reason"
+                        self.message_post(body=error_msg, subject="Mpesa Payment Transfer")
+
+            except Exception as e:
+                _logger.error("Mpesa Payment Failed during authentication",e)
+                error_msg = "Mpesa Payment Failed during authentication"
+                self.message_post(body=error_msg, subject="Mpesa Payment Auth")
 
             batch.payment_ids.write({"state": "sent"})
+
+    def _get_dfsp_id_and_type(self, payment):
+        self.ensure_one()
+        payee_id_type = self.payee_id_type
+        if payee_id_type == "bank_acc_no":
+            # TODO: Compute which bank_acc_no to choose from bank account list
+            for bank_id in payment.partner_id.bank_ids:
+                return bank_id.acc_number
+        elif payee_id_type == "bank_acc_iban":
+            # TODO: Compute which iban to choose from bank account list
+            for bank_id in payment.partner_id.bank_ids:
+                return bank_id.iban
+        elif payee_id_type == "phone":
+            return payment.partner_id.phone
+        elif payee_id_type == "email":
+            return payment.partner_id.email
+        elif payee_id_type == "reg_id":
+            for reg_id in payment.partner_id.reg_ids:
+                if reg_id.id_type.id == self.reg_id_type_for_payee_id.id:
+                    return reg_id.value
+        # TODO: Deal with no bank acc and/or ID type not matching any available IDs
+        return None
+
