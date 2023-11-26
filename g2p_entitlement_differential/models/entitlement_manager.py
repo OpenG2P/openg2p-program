b6d5757d8a8ea1e5550d0f06416d4c6d862f67a5
mkumar-02 <mkumar6@ch.iitr.ac.in>
2023-11-26 14:32:40 +0530

    Added inflation multiplier in entitlement manager config.

--- g2p_entitlement_differential/models/entitlement_manager.py
+++ g2p_entitlement_differential/models/entitlement_manager.py
@@ -1,25 +1,13 @@
 # Part of OpenG2P. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
-
-
-class EntitlementManager(models.Model):
-    _inherit = "g2p.program.entitlement.manager"
-
-    @api.model
-    def _selection_manager_ref_id(self):
-        selection = super()._selection_manager_ref_id()
-        cash_manager = ("g2p.program.entitlement.manager.cash", "Cash")
-        if cash_manager in selection:
-            selection[selection.index(cash_manager)] = (
-                "g2p.program.entitlement.manager.cash",
-                "Differential",
-            )
-        return selection
+from odoo import fields, models
 
 
 class G2PCashEntitlementManager(models.Model):
     _inherit = "g2p.program.entitlement.manager.cash"
+
+    inflation_rate = fields.Float()
+    enable_inflation = fields.Boolean(default=False)
 
     def _get_all_beneficiaries(
         self, all_beneficiaries_ids, condition, evaluate_one_item
@@ -31,6 +19,23 @@ class G2PCashEntitlementManager(models.Model):
         if evaluate_one_item:
             return beneficiaries_ids
         return all_beneficiaries_ids
+
+    def prepare_entitlements(self, cycle, beneficiaries):
+        res = super().prepare_entitlements(cycle, beneficiaries)
+
+        all_beneficiaries_ids = beneficiaries.mapped("partner_id.id")
+
+        for ben in all_beneficiaries_ids:
+            entitlement = self.env["g2p.entitlement"].search(
+                [("partner_id", "=", ben), ("cycle_id", "=", cycle.id)]
+            )
+            if entitlement:
+                if self.inflation_rate and self.enable_inflation:
+                    entitlement.initial_amount = (
+                        entitlement.initial_amount * self.inflation_rate
+                    )
+
+        return res
 
 
 class G2PCashEntitlementItem(models.Model):
