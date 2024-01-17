21d908e64fc773cb44fa716a5a59b5b1c149d832
mkumar-02 <mkumar6@ch.iitr.ac.in>
2024-01-17 09:25:43 +0530

    Fixed inkind entitlement issue.

--- g2p_entitlement_in_kind/__manifest__.py
+++ g2p_entitlement_in_kind/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://openg2p.org",
     "license": "Other OSI approved licence",
     "development_status": "Alpha",
-    "depends": ["spp_programs", "spp_entitlement_in_kind"],
+    "depends": ["spp_programs", "spp_entitlement_in_kind", "spp_service_points"],
     "data": [
         "views/in_kind.xml",
     ],
