19f1a4d187f0d4c51ca8b63a2fc6d2e0d73baa7b
Abhishek Wagh <abhishek.created20@gmail.com>
2024-04-25 15:19:46 +0530

    G2P-2157 phone deduplication and phone sanitization is working.

--- g2p_programs/models/managers/deduplication_manager.py
+++ g2p_programs/models/managers/deduplication_manager.py
@@ -438,31 +438,13 @@ class PhoneNumberDeduplication(models.Model):
         # Check Phone Numbers of each individual
         for i in group_memberships:
             for x in i.individual.phone_number_ids:
-                country_fname = "country_id"
-                number = x.phone_no
-                sanitized = str(
-                    self.env.user._phone_format(
-                        number=number,
-                        country=x.country_id,
-                        force_format="E164",
-                    )
-                )
-                phone_id_with_sanitized = {x.id: sanitized}
+                phone_id_with_sanitized = {x.id: x.phone_sanitized}
                 individual_phone_numbers.update(phone_id_with_sanitized)
 
         # Check Phone Numbers of each group
         for ix in group:
             for x in ix.phone_number_ids:
-                country_fname = "country_id"
-                number = x.phone_no
-                sanitized = str(
-                    self.env.user._phone_format(
-                        number=number,
-                        country=x.country_id,
-                        force_format="E164",
-                    )
-                )
-                phone_id_with_sanitized = {x.id: sanitized}
+                phone_id_with_sanitized = {x.id: x.phone_sanitized}
                 individual_phone_numbers.update(phone_id_with_sanitized)
 
         _logger.debug("Individual Phone Numbers: %s", individual_phone_numbers)
@@ -536,16 +518,7 @@ class PhoneNumberDeduplication(models.Model):
         # Check Phone Numbers of each individual
         for i in individuals:
             for x in i.phone_number_ids:
-                country_fname = "country_id"
-                number = x.phone_no
-                sanitized = str(
-                    self.env.user._phone_format(
-                        number=number,
-                        country=x.country_id,
-                        force_format="E164",
-                    )
-                )
-                phone_id_with_sanitized = {x.id: sanitized}
+                phone_id_with_sanitized = {x.id: x.phone_sanitized}
                 individual_phone_numbers.update(phone_id_with_sanitized)
 
         _logger.debug("Individual Phone Numbers: %s", individual_phone_numbers)
