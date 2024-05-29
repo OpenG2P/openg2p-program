efa6fb5fe3199768f2fea1429077c4cadb745895
Dibik <dibik@openg2p.org>
2024-05-30 02:49:35 +0530

    Fixed pre-commit issue

--- g2p_programs/static/src/js/hide_action_option.js
+++ g2p_programs/static/src/js/hide_action_option.js
@@ -1,17 +1,14 @@
 odoo.define("g2p_programs.BasicView", function (require) {
-    var BasicView = require('web.BasicView');
-  
+    var BasicView = require("web.BasicView");
     BasicView.include({
-        init: function(viewInfo, params) {
+        init: function (viewInfo) {
             var self = this;
             this._super.apply(this, arguments);
-            const model =  ['g2p.program_membership'] ;
-            
-            if(model.includes(self.controllerParams.modelName))
-            {
-                self.controllerParams.archiveEnabled = 'False' in viewInfo.fields;
+            const model = ["g2p.program_membership"];
+
+            if (model.includes(self.controllerParams.modelName)) {
+                self.controllerParams.archiveEnabled = "False" in viewInfo.fields;
             }
         },
     });
-
 });
