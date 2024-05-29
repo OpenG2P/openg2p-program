bd544130508cfc81ad1a4b94b0ac652cc9ee11bd
Dibik <dibik@openg2p.org>
2024-05-30 02:45:45 +0530

    G2P-1806: Removed archive & unarchive option in action bar in prgram membership model

---
+++ g2p_programs/static/src/js/hide_action_option.js
@@ -0,0 +1,17 @@
+odoo.define("g2p_programs.BasicView", function (require) {
+    var BasicView = require('web.BasicView');
+  
+    BasicView.include({
+        init: function(viewInfo, params) {
+            var self = this;
+            this._super.apply(this, arguments);
+            const model =  ['g2p.program_membership'] ;
+            
+            if(model.includes(self.controllerParams.modelName))
+            {
+                self.controllerParams.archiveEnabled = 'False' in viewInfo.fields;
+            }
+        },
+    });
+
+});
