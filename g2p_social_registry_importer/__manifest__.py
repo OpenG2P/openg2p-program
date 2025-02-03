733558c2b08b6e3bee5793d6460f2abe89edcd8c
Shibu Narayanan <shibunarayan@gmail.compwd>
2025-02-03 23:06:45 +0530

    changed the verions in the manifest and readme

--- g2p_social_registry_importer/__manifest__.py
+++ g2p_social_registry_importer/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "OpenG2P Social Registry Importer",
     "category": "G2P",
     "summary": "Import records from Social Registry",
-    "version": "17.0.1.2.0",
+    "version": "17.0.1.3.0",
     "author": "OpenG2P",
     "website": "https://openg2p.org",
     "license": "LGPL-3",
