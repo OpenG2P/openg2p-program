3a27b586a62c7617f6255a3d42b8431173a03c2e
amen50 <amenmesfin50@gmail.com>
2025-04-04 08:17:45 +0300

    Bank name

--- g2p_bridge_configuration/models/g2p_sponsoring_bank_account.py
+++ g2p_bridge_configuration/models/g2p_sponsoring_bank_account.py
@@ -5,7 +5,7 @@ class G2pSponsoringBankAccount(models.Model):
     _name = "g2p.sponsoring.bank.account"
     _description = "S2p Sponsoring Bank Account"
 
-    name = fields.Char("Bank name")
+    name = fields.Char("Account name")
     bank_code = fields.Char()
     account_number = fields.Char()
     bank_branch = fields.Char()
