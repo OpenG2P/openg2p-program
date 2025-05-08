7fb86812b06e4a3cbd56cf308400feac61949165
Manoj Kumar <mkumar6@ch.iitr.ac.in>
2025-05-08 14:43:49 +0530

    Added rabbitmq connector module
    Signed-off-by: Manoj Kumar <mkumar6@ch.iitr.ac.in>

---
+++ g2p_program_datashare_rabbitmq/README.md
@@ -0,0 +1,3 @@
+# OpenG2P Program Datashare: RabbitMQ
+
+Refer to https://docs.openg2p.org.
---
+++ g2p_program_datashare_rabbitmq/models/program_membership.py
@@ -0,0 +1,32 @@
+# Part of OpenG2P. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models
+
+
+class G2PProgramMembership(models.Model):
+    _inherit = "g2p.program_membership"
+
+    def _push_to_rabbitmq(self):
+        """Push the record to RabbitMQ after applying JQ transformation."""
+        configs = self.env["g2p.datashare.config.rabbitmq"].search(
+            [("active", "=", True), ("data_source", "=", "beneficiary_registry")]
+        )
+        for rec in self:
+            if rec.is_registrant:
+                rec_data = rec.read()[0]
+                for config in configs:
+                    transformed = config.transform_data(rec_data)
+                    config.publish(transformed)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Override create to push new records to RabbitMQ."""
+        records = super().create(vals_list)
+        records._push_to_rabbitmq()
+        return records
+
+    def write(self, vals):
+        """Override write to push updated records to RabbitMQ."""
+        res = super().write(vals)
+        self._push_to_rabbitmq()
+        return res
