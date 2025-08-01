559a61836616572dd7c3eab35d75d60f36eb57ac
Niranjan Kumar <niranjan882684@gmail.com>
2025-08-01 13:43:39 +0530

    fix: use sudo for config parameter access

--- g2p_openid_vci_programs/tests/test_vci_programs.py
+++ g2p_openid_vci_programs/tests/test_vci_programs.py
@@ -16,7 +16,7 @@ from odoo.tools import misc
 class TestVCIIssuerProgram(TransactionCase):
     def setUp(self):
         super().setUp()
-        self.env["ir.config_parameter"].set_param("web.base.url", "http://openg2p.local")
+        self.env["ir.config_parameter"].sudo().set_param("web.base.url", "http://openg2p.local")
         self.id_type = self.env["g2p.id.type"].create(
             {
                 "name": "NATIONAL ID",
